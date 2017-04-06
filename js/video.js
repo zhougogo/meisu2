41dcee78a93a1316a47c1d36140ccf7c9ea12573
zhougogo <1290612478@qq.com>
2017-04-06 11:33:00 +0800

    video
    修改

--- js/video.js
+++ js/video.js
@@ -1,9 +1,12 @@
 var canvas = document.getElementById('canvas');
 
 var ctx =canvas.getContext("2d");
-
+var i = 0;
 setInterval(function(){
 	ctx.drawImage(video, 0, 0, 304, 152);
+
+	console.log(i++);
 },1000)
+
 // context.drawImage(video, 0, 0, 330, 250);
 // console.log(canvas);
