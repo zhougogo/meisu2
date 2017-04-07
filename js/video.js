9eafaf15f4ede546a9dc791133eaa860f75a895f
zhougogo <1290612478@qq.com>
2017-04-07 16:34:29 +0800

    queue
    添加

--- js/video.js
+++ js/video.js
@@ -1,12 +1,9 @@
-var canvas = document.getElementById('canvas');
-var video = document.getElementById('video');
-var ctx =canvas.getContext("2d");
 
-video.addEventListener('play', function() {
-    setInterval(function(){
-    	ctx.drawImage(video, 0, 0, 304, 152);
-    },1000);
-},true);
+var video = document.getElementById('video');
+document.addEventListener('touchstart',function(){
+	video.play();
+})
+
 
 
 // document.addEventListener("WeixinJSBridgeReady", function onBridgeReady() {
