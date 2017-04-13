8ff6de5c15d4dcc364121bd3539fd87b288dd3c0
zhougogo <1290612478@qq.com>
2017-04-13 15:29:02 +0800

    sdjfhuridtyruiytru
    dgajhsgjdkkkkkkgfddddddddddddgfds

--- js/video.js
+++ js/video.js
@@ -7,6 +7,10 @@ var gamePic = document.getElementsByClassName('gamePic')[0];
 
 var zhezhao = document.getElementsByClassName('zhezhao')[0];
 
+var page = document.getElementById('page2');
+
+console.log(page);
+
 
 
 gamePic.addEventListener('touchstart',function(){
@@ -15,42 +19,21 @@ gamePic.addEventListener('touchstart',function(){
 	
 	gamePic.style.display = 'none';
 	
-	zhezhao.style.display = 'block';
+//	zhezhao.style.display = 'block';
 	
 	video.play();
 	
 	video.style.height = 'auto';
 
+});
+
+page.addEventListener('touchmove',function(){
+	
+	video.style.display = 'none';
+	
+	gamePic.style.display = 'block';
+	
+	console.log(123);
+	
 })
 
-//if(video.pause()){
-//	
-//	console.log(123);
-//	
-//	gamePic.style.display = 'block';
-//	
-//	video.style.display = 'block';
-//	
-//}
-
- 
-
-
-// document.addEventListener("WeixinJSBridgeReady", function onBridgeReady() {
-//     if (ua.indexOf("iPhone") > 0) {
-//         setTimeout(function(){
-//             $("#bgMusic")[0].play();
-//         },3000);
-//     }
-//     else if (ua.indexOf("Android") > 0) {
-//         var vi =  $("#bgMusic");
-//         vi[0].play();
-//         if ( vi[0].currentTime){
-//             vi[0].pause();
-//             //这里的定时器你可以不需要，也可以变成你需要的事件，而且也不一定在这个位置，主要是里面的play
-//             setTimeout(function(){
-//                 vi[0].play();
-//             },3000)
-//         }
-//     }
-// });
